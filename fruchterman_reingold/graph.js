647946dd3ec347bd92a099f43b8f3ff70d3f1812
abureyanahmed <abureyanahmed@email.arizona.edu>
2026-08-08 11:38:00 -0700

    Add files via upload

--- fruchterman_reingold/graph.js
+++ fruchterman_reingold/graph.js
@@ -243,6 +243,17 @@ function init_nodes_links_partial(nodes_main, links_main, fix_nodes, partial_lin
     centerY = y;
 }
 
+function get_adj_list(nodes_main, fix_nodes, partial_links) {
+    
+    let remaining_nodes = Object.fromEntries(
+        Object.entries(nodes_main).filter(([_, node]) => !fix_nodes.has(node.id))
+    );
+
+    // Create adjacency list using remaining nodes and partial links
+    //console.log("remaining_nodes", remaining_nodes)
+    return create_adj_list(remaining_nodes, partial_links);
+}
+
 /*function init_given_layout(){
     non_isolated_nodes = {}
     for(nodeID in nodes){
@@ -603,6 +614,111 @@ function add_nodes_on_boundary(
     }
 }
 
+function tree_height(adj_list, root) {
+    const visited = new Set();
+    const queue = [[String(root), 0]];
+    let height = 0;
+
+    visited.add(String(root));
+
+    while (queue.length > 0) {
+        const [u, depth] = queue.shift();
+
+        height = Math.max(height, depth);
+
+        for (const v of adj_list[u]) {
+            const node = String(v);
+
+            if (!visited.has(node)) {
+                visited.add(node);
+                queue.push([node, depth + 1]);
+            }
+        }
+    }
+
+    return height;
+}
+
+// compress factor = height of the tree/number of nodes in the tree
+function findCompressFactor(nodes_main, links_main, removeSource, removeTarget){
+
+    let nodeArray = Object.values(nodes_main);
+    let labelToId = Object.fromEntries(
+        nodeArray.map(node => [node.label, node.id])
+    );
+    let removeTargetId = labelToId[removeTarget];
+
+
+    //finds the component that has removeTarget
+    let partial_links = get_partial_tree(nodes_main, links_main, removeSource, removeTarget)
+
+    // make the other component vertices fixed
+    let non_fix_nodes = getNodes(partial_links)
+    let nodesArray = Object.values(nodes_main);
+    let fix_nodes_objects = new Set(nodesArray.filter(node => !non_fix_nodes.has(node.id)));
+    let fix_nodes = new Set()
+    for(let u of fix_nodes_objects){
+        fix_nodes.add(u.id)
+    }
+
+    let adj_list = get_adj_list(nodes_main, fix_nodes, partial_links)
+    //console.log(adj_list, typeof adj_list, Object.keys(adj_list).length)
+    if(Object.keys(adj_list).length==0)
+        return null
+
+    let height = tree_height(adj_list, removeTargetId)
+    //console.log(height)
+    let number_of_nodes = non_fix_nodes.size
+    //console.log(number_of_nodes)
+
+    return height/number_of_nodes
+}
+
+function findSubtreeToCompress(){
+    boundaryPoints.push({ x:1360, y:-1000 }, { x:1360, y:1940 }, { x:-300, y:1940 }, { x:-300, y:-1000 });
+    init_k_temp()
+    let {
+        non_isolated_nodes: nodes_main,
+        links: links_main
+    } = init_given_layout();
+
+
+    let removeSource = ''
+    let removeTarget = ''
+
+    let bestSource = null
+    let bestTarget = null
+    let bestCompressFactor = null
+
+    for(let link of links_main)
+    {
+        let source = link.source
+        let target = link.target
+        let currFactor = null
+
+        // which edge to remove?
+        removeSource = nodes_main[source].label
+        removeTarget = nodes_main[target].label
+        currFactor = findCompressFactor(nodes_main, links_main, removeSource, removeTarget)
+        if(currFactor>bestCompressFactor){
+            bestCompressFactor = currFactor
+            bestSource = removeSource
+            bestTarget = removeTarget
+        }
+
+        removeTarget = nodes_main[source].label
+        removeSource = nodes_main[target].label
+        currFactor = findCompressFactor(nodes_main, links_main, removeSource, removeTarget)
+        if(currFactor>bestCompressFactor){
+            bestCompressFactor = currFactor
+            bestSource = removeSource
+            bestTarget = removeTarget
+        }
+    }
+    console.log(bestSource, '-', bestTarget, bestCompressFactor);
+    
+}
+
 /*
 boundaryPoints.push({ x:10, y:15 }, { x:50, y:15 }, { x:50, y:60 }, { x:10, y:60 });
 init_k_temp()
@@ -665,6 +781,8 @@ console.log("Area:", computeBB(non_isolated_nodes))
 //Area: 1620972.755279707
 // after collision
 //Area: 1395289.2175784938
+// even better
+//Area: 1518411.8144827262
 
 //add_nodes_on_boundary(boundaryPoints, non_isolated_nodes, fix_nodes, 10);
  */
